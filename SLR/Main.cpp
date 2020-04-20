6d9d98375991adc9a88720e7d3cf07fcf3df1dc7
CaptainO5 <48147068+CaptainO5@users.noreply.github.com>
2020-04-20 17:59:30 +0530

    Conflict detection and output manip

--- SLR/Main.cpp
+++ SLR/Main.cpp
@@ -6,9 +6,10 @@ int main(){
     int icount = Items.size(), t = G.T.size(), n = G.N.size();
     string Action[icount][t];
     int Goto[icount][n];
+    bool conflict = false;
 
     int id = 0;
-    cout << "Item Sets";
+    cout << "Item Sets:\n";
     for (auto I: Items){
         cout << "I" << id++ << endl;
         for (string i: I){
@@ -25,7 +26,6 @@ int main(){
     for (int i = 0; i < icount; i++)
         for (int j = 0; j < n; j++)
             Goto[i][j] = -1;
-    
 
     for (int i = 0; i < icount; i++){
         for (string p: Items[i]){
@@ -34,15 +34,24 @@ int main(){
                 char a = p[pos+1];
                 int j = inIt(Items, GOTO(Items[i], a, G));
                 if (!isupper(a)){
-                    string val = "s";
-                    val.insert(val.end(), '5');
-                    Action[i][G.T.find(a)->second] = val;
+                    string val = "s" + to_string(j);
+                    if (Action[i][G.T.find(a)->second][0] != 'r') 
+                        Action[i][G.T.find(a)->second] = val; 
+                    else {
+                        Action[i][G.T.find(a)->second] = "C";
+                        conflict = true;
+                    }
                 } else {
                     Goto[i][G.N.find(a)->second] = j;
                 }
             } else if (p[1] != '\''){
                 for (char a: FOLLOW(p[0], G)){
-                    Action[i][G.T.find(a)->second] = "r:" + p;
+                    if (Action[i][G.T.find(a)->second] == "E") 
+                        Action[i][G.T.find(a)->second] = "r" + p; 
+                    else {
+                        Action[i][G.T.find(a)->second] = "C";
+                        conflict = true;
+                    }
                 }
             } else{
                 Action[i][G.T.find('$')->second] = "accept";
@@ -51,21 +60,29 @@ int main(){
     }
 
     cout << "\nACTION\n\nI\t";
-    for (auto x: G.T){
-        cout << x.first << "\t";
+    for (int i = 0; i < G.T.size(); i++){
+        cout << i << "\t";
     }    
     cout << endl;
 
     for (int i = 0; i < icount; i++){
         cout << i << '\t';
-        for (int j = 0; j < t; j++)
-            cout << Action[i][j] << "\t";
+        for (int j = 0; j < t; j++){
+            if (Action[i][j] != "E")
+                cout << Action[i][j];
+            cout << "\t";
+        }
         cout << endl;
     }
+    cout << "---\t";
+    for (auto x: G.T){
+        cout << x.second << ": " << x.first << "\t";
+    }    
+    cout << "---\n";
 
-    cout << "\nGOTO\n\nI\t";
-    for (auto x: G.N){
-        cout << x.first << "\t";
+    cout << "\n\nGOTO\n\nI\t";
+    for (int i = 0; i < G.N.size(); i++){
+        cout << i << "\t";
     }    
     cout << endl;
 
@@ -78,4 +95,12 @@ int main(){
         }
         cout << endl;
     }
+    cout << "---\t";
+    for (auto x: G.N){
+        cout << x.second << ": " << x.first << "\t";
+    }    
+    cout << "---\n";
+
+    if (conflict)
+        cout << "\n*** THE GRAMMAR IS NOT SLR(1) ***\n\n";
 }
